1ea2cc34680850ef945dec3e7421e5cdf8eef2f7
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 16:57:52 +0530

    Added : Program for the test uc of first name

--- src/com/blz/day20/UserRegistrationJ.java
+++ src/com/blz/day20/UserRegistrationJ.java
@@ -1,5 +1,13 @@
 package com.blz.day20;
 
-public class UserRegistrationJ {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistrationJ {
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
 }
--- src/com/blz/day20/UserRegistrationJTest.java
+++ src/com/blz/day20/UserRegistrationJTest.java
@@ -2,4 +2,21 @@ package com.blz.day20;
 
 public class UserRegistrationJTest {
 
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+public class UserRegistrationJTest {
+	UserRegistrationJ userRegistration = new UserRegistrationJ();
+
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Sandipan");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("sandipan");
+		Assert.assertEquals(false, result);
+	}
 }
