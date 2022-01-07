58edb0c7132e0e07a07bf8f85d6a2c147102c639
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 18:18:44 +0530

    Added : Program for the test the given email address

--- src/com/blz/day20/UserRegistrationJ.java
+++ src/com/blz/day20/UserRegistrationJ.java
@@ -61,4 +61,64 @@ public class UserRegistrationJ {
 		Matcher matcher = pattern.matcher(password);
 		return matcher.matches();
 	}
+
+    public boolean emailIdValidator(String emailId) {
+        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailId);
+        return matcher.matches();
+    }
+    @Test
+    public void givenEmail1_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.emailIdValidator("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail1_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmail2_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.emailIdValidator("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail3_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail4_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc123@gmail.a –");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail5_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc123@.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail6_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator(".abc@abc.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail7_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc.@gmail.com –");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail8_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc@abc@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail9_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc..2002@gmail.com");
+        Assert.assertEquals(false, result);
+    }
 }
